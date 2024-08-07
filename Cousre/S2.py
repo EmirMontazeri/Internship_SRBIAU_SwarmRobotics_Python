6518b7a01888a5681634c7f66f12aaffce964218
Emir Montazeri <137637411+EmirMontazeri@users.noreply.github.com>
2024-08-07 22:36:15 +0330

    Create S2.py

---
+++ Cousre/S2.py
@@ -0,0 +1,23 @@
+X = 'Hello Wolrd!'
+Y = X.find('lr')
+Z = X.index('r',5)
+A = 2.5
+B = 6
+B >>= 2
+
+# help(X.isidentifier)
+# help(X.maketrans)
+# help(X.__len__)
+
+# print(X.upper())
+# print(X.rjust(25))
+# print(Y,Z)
+# print(X.partition(' '))
+# print(X.split()[1])
+# print(isinstance(X,str))
+# print(B)
+# print(A!=B)
+# print(X == "Bye")
+# print(A < 3 and A < 7)
+# print(bool(A))
+print('Hello' in X.split()[1])
