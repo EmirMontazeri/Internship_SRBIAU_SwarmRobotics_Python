6b171d1f03e4b6af898d7dafd05bb002a69130fd
Emir Montazeri <137637411+EmirMontazeri@users.noreply.github.com>
2024-08-14 23:20:05 +0330

    Add files via upload

---
+++ Cousre/S4.py
@@ -0,0 +1,24 @@
+# C = ['hello','world']
+# D = ['BOOM']
+# del C[1]
+# print(C)
+# C.clear()
+# print(C)
+# C.extend(D)
+# print(C)
+
+# B = list(('Hi','hello'))
+# print(B)
+
+# A = ['A','B','C','B']
+# print('Char B index in A is {}'.format(A.index('B')))
+
+# E = ('Hi','Hello')
+# F = list(E)
+# F.append('Bye')
+# print(tuple(F))
+
+A = ['Behrad chossuuuu']
+B= ['Nude bedeeeeeeee']
+C = str(A + B)
+print('ye payam daram;', C)
