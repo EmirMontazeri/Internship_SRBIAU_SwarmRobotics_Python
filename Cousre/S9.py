10abfd09c9aaedd52dc2a01efecfd777b3cd9a6e
Emir Montazeri <137637411+EmirMontazeri@users.noreply.github.com>
2024-08-21 23:37:59 +0330

    Update S9.py

--- Cousre/S9.py
+++ Cousre/S9.py
@@ -7,3 +7,13 @@ A = {
 y = json.dumps(A)
 print(y)
 print(json.dumps(y, indent = 4))
+
+import re
+txt = 'My car leak oil'
+x = re.search('oil$', txt)
+if x:
+    print('It needs a service')
+else:
+    print('All good')
+
+print(dir(re))
